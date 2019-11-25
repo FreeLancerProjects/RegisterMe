bbabc8be87e58c1b98ff0c7ea2df33ff594c6fe0
ARAB <ahmedmohamed23345@gmail.com>
2019-11-25 13:59:34 +0200

    ch

--- app/src/main/java/com/creativeshare/registerme/tags/Tags.java
+++ app/src/main/java/com/creativeshare/registerme/tags/Tags.java
@@ -2,7 +2,7 @@ package com.creativeshare.registerme.tags;
 
 public class Tags {
 
-    public static String base_url = "http://registerme.creativeshare.sa/";
+    public static String base_url = "http://rawayie.com.sa/";
     public static String IMAGE_URL = base_url + "upload/";
     public static final String IMAGE_SLIDER_URL = base_url + "upload/sliders/";
     public static final String custom_url = base_url + "upload/customs_clearances/";
